932281e7e6c69b43e390abb2df2252ca13dd9fd2
Haider Zaman <hzaman@code-freaks.com>
2024-02-29 02:22:46 +0500

    updated the footer and made other changes

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Testimonials = () => {
   return (
     <>
-      <div className="min-w-screen min-h-screen bg-gray-50 flex items-center justify-center">
+      <div className="min-w-screen min-h-screen md:bg-gray-100 flex items-center justify-center">
         <div className="w-full bg-white border-t border-b border-gray-200 px-5 py-16 md:py-24 text-gray-800">
           <div className="w-full max-w-6xl mx-auto">
             <div className="text-center max-w-xl mx-auto">
